1b30cfe3464c7cc4711ded8c6246312aea32a286
ookangzheng <okz5289@gmail.com>
2019-01-10 03:04:26 +0800

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,62 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import Icon from "@material-ui/core/Icon";
+import Nav from "./Nav";
+
+const styles = () => ({
+  root: {
+    width: "100%",
+    maxWidth: 360
+  }
+});
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div />;
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Nav />
+        <List className={classes.root}>
+          <ListItem>
+            <Avatar>
+              <Icon>mail</Icon>
+            </Avatar>
+            <ListItemText
+              primary="Send email"
+              secondary="Send an email if you have any questions or suggestions"
+            />
+          </ListItem>
+          <ListItem>
+            <Avatar>
+              <Icon>star</Icon>
+            </Avatar>
+            <ListItemText
+              primary="Rate us"
+              secondary="Love it? Give us a Star"
+            />
+          </ListItem>
+          <ListItem>
+            <Avatar>
+              <Icon>facebook</Icon>
+            </Avatar>
+            <ListItemText primary="Facebook" secondary="Find us on facebook" />
+          </ListItem>
+        </List>
+      </React.Fragment>
+    );
   }
 }
+About.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
-export default About;
+export default withStyles(styles)(About);
