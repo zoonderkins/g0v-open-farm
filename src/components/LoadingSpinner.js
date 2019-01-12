6ddae5694557428b5d7a19677e549fae2b16c07f
yuanyu90221 <yuanyu90221@gmail.com>
2019-01-12 21:55:22 +0800

    update css of spin loading

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,14 +1,5 @@
 import React, {Component} from 'react';
-import {css} from '@emotion/core';
 import {DotLoader} from 'react-spinners';
-
-const override = css`
-  display: block;
-  margin: 10vh auto;
-  border-color: red;
-  height: 100%;
-  width: 100%;
-`;
 
 class LoadingSpinner extends Component{
   render() {
@@ -16,8 +7,8 @@ class LoadingSpinner extends Component{
     return (
       <div className="sweet-loading">
         <DotLoader
-          css={override}
-          sizeUnit={"vw"}
+          css={'display:block; margin: 10vh auto;'}
+          sizeUnit={"vw"}          
           size={50}
           color={"#123abc"}
           loading={loading}
