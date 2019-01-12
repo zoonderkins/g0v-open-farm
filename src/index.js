6f995d836300321cb59b031bc16d4a91099c686c
ookangzheng <okz5289@gmail.com>
2019-01-13 02:43:47 +0800

    index add  process based url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
