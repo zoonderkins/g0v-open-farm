c62e5a5078c88ffb3cf7df088e2090e32711b406
yuanyu90221 <yuanyu90221@gmail.com>
2019-01-13 00:52:18 +0800

    add fetchMethod

--- src/util/apiLogic.js
+++ src/util/apiLogic.js
@@ -8,6 +8,32 @@ import {apiUrl} from '../config/config.json';
 const getVegatableList = async ({numToFetch=10, filterFn=null}) => {
   // load data from apiUrl
   try {
+    let finalResult = await fetchMethod(`${apiUrl}`); 
+    if (filterFn!=null && typeof filterFn === "function" ) {
+      finalResult = finalResult.filter((item)=>filterFn(item));
+    }
+    // only list previous numToFetch items
+    finalResult = finalResult.filter((item)=> {
+      return (item.cover != null && item.avg_total_growing_days!= null && item.min_growing_temperature!= null
+      && item.height!=null) || (item.max_pH!=null && item.min_pH!=null && item.variety!=null);
+    });
+    finalResult = finalResult.slice(0, numToFetch);
+    console.log(`[getVegatableList] api data:`,finalResult);
+    return finalResult;
+  } catch (e) {
+    throw e;
+  }
+};
+/**
+ * @description fetchMethod
+ * 
+ * @param {string} apiUrl 
+ */
+const fetchMethod = async (apiUrl) => {
+  try {
+    if (apiUrl===undefined||apiUrl===""){
+      throw new Error(`apiUrl missing: ${apiUrl}`);
+    }
     let result = await fetch(`${apiUrl}`);
     let finalResult = null;
     if (result.status === 200) {
@@ -19,17 +45,7 @@ const getVegatableList = async ({numToFetch=10, filterFn=null}) => {
     } else {
       finalResult = await result.text();
       throw new Error(finalResult);
-    } 
-    if (filterFn!=null && typeof filterFn === "function" ) {
-      finalResult = finalResult.filter((item)=>filterFn(item));
     }
-    // only list previous numToFetch items
-    finalResult = finalResult.filter((item)=> {
-      return (item.cover != null && item.avg_total_growing_days!= null && item.min_growing_temperature!= null
-      && item.height!=null) || (item.max_pH!=null && item.min_pH!=null && item.variety!=null);
-    });
-    finalResult = finalResult.slice(0, numToFetch);
-    console.log(`[getVegatableList] api data:`,finalResult);
     return finalResult;
   } catch (e) {
     throw e;
